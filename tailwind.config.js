2d490354cc006f924d3b3e65c1b64e515df4cc4e
Mani Yeganeh <yeganehmani@gmail.com>
2023-04-25 09:11:53 +0330

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "wh-10": "#f4f4f4",
+        "wh-50": "#fbfbfb",
+        "wh-100": "#939393",
+        "wh-300": "#c9c9c9",
+        "wh-500": "#595959",
+        "wh-900": "#0f0f0f",
+        "accent-red": "#ea9648",
+        "accent-orange": "#f6cf68",
+        "accent-green": "#c2e984"
       },
+      backgroundImage: (theme) => ({
+        "gradient-gradiual":
+          "linear-gradient(180deg , rgba(116 , 116 , 116 , 0) 66.15%,#0000 , 100%)"
+
+      }),
     },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px"
+    }
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography")
+  ],
 }
